5ba3eb7032bc5ed8f0aaf2810b5ceb78c3c103f9
jayantrathi <jayantrathi955@gmail.com>
2021-12-25 15:24:44 +0530

    New Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'views/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.white,
       ),
-      home: Container(),
+      home: Home(),
     );
   }
 }
